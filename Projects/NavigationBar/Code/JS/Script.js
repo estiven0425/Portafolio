f9c9d139683dc899912f4c205faf253d2806b311
estiven0425 <estivenmoto6@gmail.com>
2024-03-24 20:04:25 -0500

    Inicio de proyecto CHRONO

--- Projects/NavigationBar/Code/JS/Script.js
+++
@@ -1,9 +0,0 @@
-function HideShowSection() {
-    let Section = document.querySelector('.HideSection');
-    
-    if(Section.classList.contains('HideSection')) {
-        Section.classList.toggle('ShowSection');
-    } else {
-        Section.classList.toggle('HideSection');
-    }
-}
