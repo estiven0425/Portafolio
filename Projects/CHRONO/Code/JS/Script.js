b76a3a77ef3ab5f9b624064e632742142a246c62
estiven0425 <estivenmoto6@gmail.com>
2024-03-27 16:59:55 -0500

    Sección 3

---
+++ Projects/CHRONO/Code/JS/Script.js
@@ -0,0 +1,12 @@
+// ---------- Localizadores ---------- //
+const FeaturedProductsSection = document.getElementById('FeaturedProductsSection');
+const LeftButton = document.getElementById('LeftFeaturedProductsSection');
+const RightButton = document.getElementById('RightFeaturedProductsSection');
+
+// ---------- Slider ---------- //
+LeftButton.addEventListener('click', function () {
+    FeaturedProductsSection.scrollLeft -= 450;
+});
+RightButton.addEventListener('click', function () {
+    FeaturedProductsSection.scrollLeft += 450;
+});
