cb359704e7c3fc2d1a0bb8d5985447272982fe6f
estiven0425 <estivenmoto6@gmail.com>
2024-03-28 17:36:32 -0500

    Avance proyecto CHRONO

--- Projects/CHRONO/Code/JS/Script.js
+++ Projects/CHRONO/Code/JS/Script.js
@@ -2,7 +2,6 @@
 const FeaturedProductsSection = document.getElementById('FeaturedProductsSection');
 const LeftButton = document.getElementById('LeftFeaturedProductsSection');
 const RightButton = document.getElementById('RightFeaturedProductsSection');
-
 // ---------- Slider ---------- //
 LeftButton.addEventListener('click', function () {
     FeaturedProductsSection.scrollLeft -= 450;
