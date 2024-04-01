6ae9b6a33a4a9bd09adafd4b796c4a261e46451f
estiven0425 <estivenmoto6@gmail.com>
2024-03-31 20:23:20 -0500

    Avance proyecto Chrono

--- Projects/CHRONO/Code/JS/Script.js
+++ Projects/CHRONO/Code/JS/Script.js
@@ -1,11 +1,21 @@
 // ---------- Localizadores ---------- //
-const FeaturedProductsSection = document.getElementById('FeaturedProductsSection');
-const LeftButton = document.getElementById('LeftFeaturedProductsSection');
-const RightButton = document.getElementById('RightFeaturedProductsSection');
-// ---------- Slider ---------- //
-LeftButton.addEventListener('click', function () {
+const FeaturedProductsSection = document.getElementById('FeaturedProductsSectionMain');
+const LeftButtonFeatured = document.getElementById('LeftFeaturedProductsSectionMain');
+const RightButtonFeatured = document.getElementById('RightFeaturedProductsSectionMain');
+const TestimonialsSection = document.getElementById('BottomTestimonialsSectionMain');
+const LeftButtonTestimonials = document.getElementById('LeftButtonBottomTestimonialsSectionMain');
+const RightButtonTestimonials = document.getElementById('RightButtonBottomTestimonialsSectionMain');
+// ---------- Slider featured ---------- //
+LeftButtonFeatured.addEventListener('click', function () {
     FeaturedProductsSection.scrollLeft -= 450;
 });
-RightButton.addEventListener('click', function () {
+RightButtonFeatured.addEventListener('click', function () {
     FeaturedProductsSection.scrollLeft += 450;
+});
+// ---------- Slider testimonials ---------- //
+LeftButtonTestimonials.addEventListener('click', function () {
+    TestimonialsSection.scrollLeft -= 1500;
+});
+RightButtonTestimonials.addEventListener('click', function () {
+    TestimonialsSection.scrollLeft += 1500;
 });
